a2413e2d698ccd0bb5e679801a993ad5ba9406e8
Saso Miocinovic <saso.miocinovic@gmail.com>
2015-10-08 18:27:34 +0200

    Changed connection string in web.config.

--- OdeToFood/Models/OdeToFoodDb.cs
+++ OdeToFood/Models/OdeToFoodDb.cs
@@ -2,6 +2,7 @@
 using System.Collections.Generic;
 using System.Data.Entity;
 using System.Linq;
+using System.Security.Cryptography.X509Certificates;
 using System.Web;
 
 namespace OdeToFood.Models
@@ -10,6 +11,10 @@ namespace OdeToFood.Models
     //DbContext
     public class OdeToFoodDb : DbContext
     {
+        public OdeToFoodDb() : base("name=DefaultConnection")
+        {
+            
+        }
         /*We have properties of type DbSet that represent
          * the entities we want to query and PERSIST*/
         public DbSet<Restaurant> Restaurants { get; set; }
