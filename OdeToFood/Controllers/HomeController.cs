a57b7ad186f351782ea2aca1459d00ec321c27cf
Saso Miocinovic <saso.miocinovic@gmail.com>
2015-10-14 17:51:52 +0200

    Same query in comprehension syntax.

--- OdeToFood/Controllers/HomeController.cs
+++ OdeToFood/Controllers/HomeController.cs
@@ -16,19 +16,35 @@ namespace OdeToFood.Controllers
             //We retrieve all restaurants from Restaurants
             
             //Select restaurant based on average review
-            var model =
-                from r in _db.Restaurants
-                orderby r.Reviews.Average(review => review.Rating) descending 
-                select new RestaurantListViewModel
+            //var model =
+            //    from r in _db.Restaurants
+            //    orderby r.Reviews.Average(review => review.Rating) descending 
+            //    select new RestaurantListViewModel
+            //    {
+            //        //Id = r.Id,
+            //        //Name = r.Name,
+            //        //City = r.City,
+            //        //Country = r.Country,
+            //        ////Anon type - to add the reviews which arent in this model
+            //        //CountOfReviews = r.Reviews.Count()
+            //        
+            //        
+            //        
+            //    };
+            var model = _db.Restaurants
+                .OrderByDescending(r => r.Reviews.Average(review => review.Rating))
+                .Select(r => new RestaurantListViewModel()
                 {
                     Id = r.Id,
                     Name = r.Name,
                     City = r.City,
                     Country = r.Country,
-                    //Anon type - to add the reviews which arent in this model
                     CountOfReviews = r.Reviews.Count()
-                };
-            
+
+                }
+                );
+
+
             return View(model);
         }
 
