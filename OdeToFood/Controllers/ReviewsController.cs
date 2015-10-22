938a0204552678c6d94f59479a66a7cae178e285
Saso Miocinovic <saso.miocinovic@gmail.com>
2015-10-22 17:28:44 +0200

    Deleted in-memory data for Reviews Controller leaving only Index action.

--- OdeToFood/Controllers/ReviewsController.cs
+++ OdeToFood/Controllers/ReviewsController.cs
@@ -9,57 +9,17 @@ namespace OdeToFood.Controllers
 {
     public class ReviewsController : Controller
     {
-        [ChildActionOnly]
-        public ActionResult BestReview()
-        {
-            var bestReview = from r in _reviews
-                             orderby r.Rating descending
-                             select r;
-
-            return PartialView("_Review", bestReview.First());
-        }
-        // GET: /Reviews/
+        
 
         public ActionResult Index()
         {
             var model = from r in _reviews
-                        orderby r.Country
-                        select r;
+                orderby r.Country
+                select r;
 
 
             return View(model);
         }
-        static List<RestaurantReview> _reviews = new List<RestaurantReview>
-        {
-            new RestaurantReview
-            {
-                Id = 1,
-                Name = "Cinnamon Club",
-                City = "London",
-                Country = "UK",
-                Rating = 10
-            },
-            new RestaurantReview
-            {
-                Id = 2,
-                Name = "Marakesh",
-                City = "D.C.",
-                Country = "USA",
-                Rating = 10
-            },
-            new RestaurantReview
-            {
-                Id = 3,
-                Name = "The House of Elliot",
-                City = "Ghet",
-                Country = "Belgium",
-                Rating = 10
-
-                
-            }
-
-        };
-
     }
 
 }
