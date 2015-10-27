e1f0c34d97f16bd97dcbbe6f79375fc40d82bd40
Saso Miocinovic <saso.miocinovic@gmail.com>
2015-10-27 15:23:42 +0100

    Removed Validation Attribute.

--- OdeToFood/Models/RestaurantReview.cs
+++ OdeToFood/Models/RestaurantReview.cs
@@ -6,34 +6,7 @@ using System.Web;
 
 namespace OdeToFood.Models
 {
-    /*1st we derive from base class ValidationAttribute*/
-    public class MaxWordsAttribute : ValidationAttribute
-    {
-        public MaxWordsAttribute(int maxWords) :
-            base ("{0} has too many words")
-        {
-            _maxWords = maxWords;
-        }
-        /*We override method IsValid*/
-        /*ModelBinder will pass in reviewer name eg as value*/
-        protected override ValidationResult IsValid(object value,ValidationContext validationContext)
-        {
-            if (value != null)
-            {
-                var valueAsString = value.ToString();
-                /*We're simply checking how many spaces are in there and 
-                 putting it to array*/
-                if (valueAsString.Split(' ').Length > _maxWords)
-                {
-                    var errorMessage = FormatErrorMessage(validationContext.DisplayName);
-                    return new ValidationResult(ErrorMessage);
-                }
-            }
-            return ValidationResult.Success;
-        }
-
-        private readonly int _maxWords;
-    }
+   
     public class RestaurantReview
     {
         public int Id { get; set; }
