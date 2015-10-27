bdb5d234aa0f8f8119e0f60d61fcdbf22541031d
Saso Miocinovic <saso.miocinovic@gmail.com>
2015-10-27 12:28:26 +0100

    Using data annotations to provide required fields and value checking. Also a slight change in model and DB migration.

--- OdeToFood/Models/RestaurantReview.cs
+++ OdeToFood/Models/RestaurantReview.cs
@@ -1,5 +1,6 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
 using System.Linq;
 using System.Web;
 
@@ -8,9 +9,17 @@ namespace OdeToFood.Models
     public class RestaurantReview
     {
         public int Id { get; set; }
+        /*Data annotation for range*/
+        [Range(1,10)]
+        /*Required is redundant, value types cannot
+         be left null!*/
+        [Required]
         public int Rating { get; set; }
         //For storing text
+        [Required]
+        [StringLength(1024)]
         public string Body { get; set; }
+        
         public string Reviewer { get; set; }
         public int RestaurantId { get; set; }
     }
