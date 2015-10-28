887b8b55d2236aa4cd1f5eb8b3c5c3536cdb2272
Saso Miocinovic <saso.miocinovic@gmail.com>
2015-10-28 12:33:28 +0100

    Bundeled some more JS files and refactored script rendering across several pages.

--- OdeToFood/App_Start/BundleConfig.cs
+++ OdeToFood/App_Start/BundleConfig.cs
@@ -8,6 +8,11 @@ namespace OdeToFood
         // For more information on Bundling, visit http://go.microsoft.com/fwlink/?LinkId=254725
         public static void RegisterBundles(BundleCollection bundles)
         {
+            bundles.Add(new ScriptBundle("~/bundles/oft").Include(
+                "~/Scripts/jquery-{version}.js",
+                "~/Scripts/jquery-ui-{version}.js",
+                "~/Scripts/jquery.unobtrusive*",
+                "~/Scripts/jquery.validate*"));
             bundles.Add(new ScriptBundle("~/bundles/jquery").Include(
                         "~/Scripts/jquery-{version}.js"));
 
