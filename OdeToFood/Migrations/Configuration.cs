503789b2f9aea744c5cc1b348a9a4eb3580801ac
Saso Miocinovic <saso.miocinovic@gmail.com>
2015-10-28 12:22:22 +0100

    Seeded DB with 1000 random restaurant instances.

--- OdeToFood/Migrations/Configuration.cs
+++ OdeToFood/Migrations/Configuration.cs
@@ -29,6 +29,16 @@ namespace OdeToFood.Migrations
                         new RestaurantReview{Rating = 9, Body = "Great Food!",Reviewer = "Scott"}
                     }
                 });
+            for (int i = 0; i < 1000; i++)
+            {
+                context.Restaurants.AddOrUpdate(r=>r.Name,
+                    new Restaurant
+                    {
+                        Name = i.ToString(),
+                        City = "Nowhere",
+                        Country = "USA"
+                    });
+            }
         }
     }
 }
