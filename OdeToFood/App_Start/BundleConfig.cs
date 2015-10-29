c331794c3986586dac15a94343670e766e4c8f46
Saso Miocinovic <saso.miocinovic@gmail.com>
2015-10-29 23:04:41 +0100

    Added some styling to PagedList.

--- OdeToFood/App_Start/BundleConfig.cs
+++ OdeToFood/App_Start/BundleConfig.cs
@@ -29,7 +29,7 @@ namespace OdeToFood
             bundles.Add(new ScriptBundle("~/bundles/modernizr").Include(
                         "~/Scripts/modernizr-*"));
 
-            bundles.Add(new StyleBundle("~/Content/css").Include("~/Content/site.css"));
+            bundles.Add(new StyleBundle("~/Content/css").Include("~/Content/site.css", "~/Content/PagedList.css"));
 
             bundles.Add(new StyleBundle("~/Content/themes/base/css").Include(
                         "~/Content/themes/base/jquery.ui.core.css",
